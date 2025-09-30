76f20b425fd7f82c79253201016ee50e72ba5f48
we9lii <wesli808@gmail.com>
2025-09-30 17:09:36 +0300

    إصلاحات

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+console.log("Loading routes...");
 const authRoutes = require('./routes/auth.js');
+console.log("-> auth.js loaded.");
 const reportRoutes = require('./routes/reports.js');
+console.log("-> reports.js loaded.");
 const userRoutes = require('./routes/users.js');
+console.log("-> users.js loaded.");
 const branchRoutes = require('./routes/branches.js');
+console.log("-> branches.js loaded.");
 const workflowRoutes = require('./routes/workflow.js');
+console.log("-> workflow.js loaded.");
+console.log("All routes loaded successfully.");
 
 const app = express();
 
