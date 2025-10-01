1161776d0e70f0368d724ee9d7273873a7d38333
we9lii <wesli808@gmail.com>
2025-10-01 09:11:17 +0300

    index

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const authRoutes = require('./auth.js');
 const reportRoutes = require('./reports.js');
 const userRoutes = require('./users.js');
 const branchRoutes = require('./branches.js');
-const workflowsRoutes = require('./workflows.js');
+const workflowsRoutes = require('./workflows.js'); // Corrected file name
 
 router.use(authRoutes);
 router.use(reportRoutes);
 router.use(userRoutes);
 router.use(branchRoutes);
-router.use(workflowsRoutes);
+router.use(workflowsRoutes); // Using the correct routes
 
 module.exports = router;
